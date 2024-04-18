61a28c8f093bd99f35cff0948e6edd2afd925508
Thibault Fouquaert <thibault.fouquaert@gmail.com>
2024-04-18 16:54:56 +0200

    added minify to typedJS

---
+++ src/assets/js/jsTyped.js
@@ -0,0 +1,9 @@
+let typed = new Typed('#typed', {
+  stringsElement: '#typed-strings',
+  typeSpeed: 40,
+  backSpeed: 20,
+  shuffle: true,
+  smartBackspace: false,
+  loop: true,
+  backDelay: 1500,
+})
