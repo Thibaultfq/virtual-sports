28a26deefc2c538cbd3cd1ad628e4fafee7af142
Thibault Fouquaert <thibault.fouquaert@gmail.com>
2024-04-18 11:06:00 +0200

    No lazy loading on videos of about page

--- utils/paired-shortcodes.js
+++ utils/paired-shortcodes.js
@@ -15,7 +15,7 @@ module.exports = {
           ${content}
         </div>
         <div class="grow sm:basis-5/12 relative self-center">
-          <video muted loop lazy
+          <video muted loop
           x-data="{}" x-intersect:enter.margin.-35%.0="$el.play()" x-intersect:leave.margin.-35%.0="$el.pause()" onclick="this.paused?this.play():this.pause()" ontouchend="this.paused?this.play():this.pause()" 
           class="object-cover w-full rounded-2xl [clip-path:inset(1px_1px)]" 
           alt="${videoAlt}" >
