5a7d64e702a93dfee04b404c5a4ca2b09ce88421
Thibault Fouquaert <thibault.fouquaert@gmail.com>
2024-04-23 17:49:31 +0200

    added network to main nav

--- src/_data/structure.js
+++ src/_data/structure.js
@@ -65,7 +65,7 @@ module.exports = {
    * *** before this file existed. :-)
    * I might move stuff around on the next version...
    */
-  g_showFooterSocialIcons: true,
+  g_showFooterNav: true,
   /**
    * Set to 'false' to disable the "Share:" icons on the Post Detail views.
    * You can also set individual platforms to 'false' to disable just those platforms.
