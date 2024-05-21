ac1796314b2d64c0e6bfc1913255f24b2a221e71
Thibault Fouquaert <thibault.fouquaert@gmail.com>
2024-05-21 18:54:24 +0200

    changed youtube shortcode

--- utils/shortcodes.js
+++ utils/shortcodes.js
@@ -82,16 +82,19 @@ module.exports = {
        * you only need the following:
        *  {% youtube "1234567" %}
        */
-      youtube: function (vid, vtitle, vratio, vstart) {
+      youtube: function (vid, vtitle, vclasses, vratio, vstart) {
         const slug = vid ? vid : 'oHg5SJYRHA0'
         const title = vtitle ? vtitle : 'YouTube Video'
         const ratio = vratio ? vratio : '16:9'
+        const classes = vclasses ? classes : ''
         const start = vstart ? vstart.split(':').reduce((minute, seconds) => Number(minute) * 60 + Number(seconds)) : ''
         const padding = ratio.split(':').reduce((first, second) => (second / first) * 100)
 
-        return `<div id="${slug}" style="position:relative; width:100%; padding-bottom:${padding}%"><iframe style="position:absolute; top:0; right:0; bottom:0; left:0; width:100%; height:100%" width="100%" height="100%" title="${title}" src="https://www.youtube.com/embed/${slug}${
+        return `
+        <div class="lg:w-3/4 2xl:w-4/6 mx-auto ${classes}" >
+        <div id="${slug}" style="position:relative; width:100%; padding-bottom:${padding}%"><iframe style="position:absolute; top:0; right:0; bottom:0; left:0; width:100%; height:100%" width="100%" height="100%" title="${title}" src="https://www.youtube.com/embed/${slug}${
           start ? `?start=${start}` : ''
-        }" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen loading="lazy"></iframe></div>`
+        }" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen loading="lazy"></iframe></div></div>`
       },
     }
   },
