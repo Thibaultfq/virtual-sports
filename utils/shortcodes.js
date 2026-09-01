443b2cf5952474a707e0cd4894faace5f03f0f4e
Thibault Fouquaert <thibault.fouquaert@gmail.com>
2026-09-01 16:18:21 +0200

    updated first text block homepage

--- utils/shortcodes.js
+++ utils/shortcodes.js
@@ -30,36 +30,29 @@ export function init(eleventyConfig, markdownLib) {
 
     ctaButton: function (content, classes, type = 'button', href = '') {
       //to access variables in nunjuncks, use this.ctx in filters and shortcodes
-      return `${
-        href ? '<a href=' + href + '>' : ''
-      }<button type="${type}" class="text-2xl md:text-3xl px-3 py-2 text-balance font-medium rounded-md ${
-        this.ctx.colors.buttonDefault.text || this.ctx.colors.buttonCustom.text
-      } ${this.ctx.colors.buttonDefault.textHover || this.ctx.colors.buttonCustom.textHover} ${
-        this.ctx.colors.buttonDefault.bg || this.ctx.colors.buttonCustom.bg
-      } ${this.ctx.colors.buttonDefault.bgHover || this.ctx.colors.buttonCustom.bgHover} ${
-        this.ctx.colors.buttonDefault.border || this.ctx.colors.buttonCustom.border
-      } ${classes ? classes : ''}">${content}
+      return `${href ? '<a href=' + href + '>' : ''
+        }<button type="${type}" class="cursor-pointer text-2xl md:text-3xl px-3 py-2 text-balance font-medium rounded-md ${this.ctx.colors.buttonDefault.text || this.ctx.colors.buttonCustom.text
+        } ${this.ctx.colors.buttonDefault.textHover || this.ctx.colors.buttonCustom.textHover} ${this.ctx.colors.buttonDefault.bg || this.ctx.colors.buttonCustom.bg
+        } ${this.ctx.colors.buttonDefault.bgHover || this.ctx.colors.buttonCustom.bgHover} ${this.ctx.colors.buttonDefault.border || this.ctx.colors.buttonCustom.border
+        } ${classes ? classes : ''}">${content}
         </button>
         ${href ? '</a>' : ''}`
     },
 
     heading: function (content, subheading, classes = '', anchorLink = true) {
       const h3 = subheading
-        ? `<h3 class="${
-            this.ctx.colors.headingsCustom || this.ctx.colors.headingsDefault
-          } mb-6 text-center italic font-normal text-2xl ">${markdownLib.renderInline(subheading.trim())}</h3>`
+        ? `<h3 class="${this.ctx.colors.headingsCustom || this.ctx.colors.headingsDefault
+        } mb-6 text-center italic font-normal text-2xl ">${markdownLib.renderInline(subheading.trim())}</h3>`
         : ''
 
       const a = anchorLink
-        ? `<a class="${
-            structure.g_markdownItAnchor_classes
-          } absolute inset-y-50	 left-100 ml-2" href="#${eleventyConfig.getFilter('slugify')(content)}"
+        ? `<a class="${structure.g_markdownItAnchor_classes
+        } absolute inset-y-50	 left-100 ml-2" href="#${eleventyConfig.getFilter('slugify')(content)}"
             title="link to the subtitle: ${content.trim()}">#</a>`
         : ''
 
-      return `<h2 class="w-full mb-6 text-5xl font-bold text-center text-balance ${
-        this.ctx.colors.headingsCustom || this.ctx.colors.headingsDefault
-      } ${anchorLink ? 'relative px-10' : ''} ${classes}"
+      return `<h2 class="w-full mb-6 text-5xl font-bold text-center text-balance ${this.ctx.colors.headingsCustom || this.ctx.colors.headingsDefault
+        } ${anchorLink ? 'relative px-10' : ''} ${classes}"
         ${anchorLink ? `tabindex="-1"` : ''} 
         ${anchorLink ? `id="${eleventyConfig.getFilter('slugify')(content)}"` : ''}>
         ${markdownLib.renderInline(content.trim())}
@@ -92,9 +85,8 @@ export function init(eleventyConfig, markdownLib) {
       const padding = ratio.split(':').reduce((first, second) => (second / first) * 100)
 
       return `<div class="${classes}" >
-        <div id="${slug}" style="position:relative; width:100%; padding-bottom:${padding}%"><iframe style="position:absolute; top:0; right:0; bottom:0; left:0; width:100%; height:100%" width="100%" height="100%" title="${title}" src="https://www.youtube.com/embed/${slug}${
-        start ? `?start=${start}` : ''
-      }" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen loading="lazy"></iframe></div></div>`
+        <div id="${slug}" style="position:relative; width:100%; padding-bottom:${padding}%"><iframe style="position:absolute; top:0; right:0; bottom:0; left:0; width:100%; height:100%" width="100%" height="100%" title="${title}" src="https://www.youtube.com/embed/${slug}${start ? `?start=${start}` : ''
+        }" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen loading="lazy"></iframe></div></div>`
     },
 
     exergameEsportScorebar: function (score) {
